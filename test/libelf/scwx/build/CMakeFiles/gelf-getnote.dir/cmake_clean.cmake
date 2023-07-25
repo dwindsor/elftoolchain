e22c532b004257f41ec428fd465e112eb3d4a9a6
David Windsor <dwindsor@secureworks.com>
2023-07-25 10:06:56 -0400

    Add getnote test
    Signed-off-by: David Windsor <dwindsor@secureworks.com>

---
+++ test/libelf/scwx/build/CMakeFiles/gelf-getnote.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gelf-getnote.dir/gelf-getnote.c.o"
+  "gelf-getnote"
+  "gelf-getnote.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gelf-getnote.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
